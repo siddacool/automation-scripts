9fbfa543f2aa0c28a24c969ed6645c0fe54f31f3
Siddhesh Mangela <sidthecool007@gmail.com>
2022-12-22 10:52:19 +0530

    fix page refresh required on page

--- src/youtube-enter-play-fullscreen-tm.js
+++ src/youtube-enter-play-fullscreen-tm.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         youtube-enter-play-fullscreen
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Assign Enter as play button on full screen
 // @author       Sid
-// @match        https://www.youtube.com/watch?*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.youtube.com/
 // @grant        none
 // ==/UserScript==
@@ -19,7 +19,11 @@
   };
 
   const btnAssigner = (event) => {
-    if (event.key === 'Enter' && document.fullscreenElement !== null) {
+    if (
+      event.key === 'Enter' &&
+      document.fullscreenElement !== null &&
+      window.location.href.includes('watch')
+    ) {
       event.preventDefault();
 
       triggerSpace();
